8ad19a52535472870cc2de66e0cf07f62c8954cf
calvinkurniawann <calvinnkurniawan@gmail.com>
2026-02-08 16:03:28 +0700

    feat(booking): complete CRUD Room Booking (#2)

--- Controllers/RoomBookingsController.cs
+++ Controllers/RoomBookingsController.cs
@@ -24,13 +24,90 @@ namespace ManajemenRuangan.Controllers
             return Ok(data);
         }
 
+        // GET: api/bookings/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var booking = await _context.RoomBookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound(new { Message = "Data tidak ditemukan" });
+            }
+            return Ok(booking);
+        }
+
+
+
         // POST: api/bookings
         [HttpPost]
         public async Task<IActionResult> Create(RoomBooking booking)
         {
+            booking.Status = BookingStatus.Pending;
+
             _context.RoomBookings.Add(booking);
             await _context.SaveChangesAsync();
+
             return Ok(booking);
+        }
+
+        // PUT: api/bookings/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, RoomBooking updated)
+        {
+            var booking = await _context.RoomBookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            if (booking.Status != BookingStatus.Pending)
+            {
+                return BadRequest(new { Message = "Tidak bisa mengubah data yang sudah diproses" });
+            }
+
+            booking.RoomName = updated.RoomName;
+            booking.BorrowerName = updated.BorrowerName;
+            booking.Date = updated.Date;
+            booking.Purpose = updated.Purpose;
+            booking.Status = updated.Status;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(booking);
+        }
+
+
+
+        //PUT api/bookings/{id}/status
+        [HttpPut("{id}/status")]
+        public async Task<IActionResult>UpdateStatus(int id, [FromBody] BookingStatus status)
+        {
+            var booking = await _context.RoomBookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            booking.Status = status;
+            await _context.SaveChangesAsync();
+
+            return Ok(booking);
+        }
+
+        // DELETE: api/bookings/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var booking = await _context.RoomBookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _context.RoomBookings.Remove(booking);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
