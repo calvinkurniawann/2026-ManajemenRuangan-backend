f095cbb089b8fc3e55dbdf43dcde23fd83b0dbd2
calvinkurniawann <calvinnkurniawan@gmail.com>
2026-02-10 16:54:28 +0700

    feat(db): seed initial room data (#11)

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,11 +7,34 @@ namespace ManajemenRuangan.Data
     {
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<RoomBooking>()
                 .HasOne(rb => rb.Room)
                 .WithMany(r => r.RoomBookings)
                 .HasForeignKey(rb => rb.RoomId)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    id = 1,
+                    Name = "Ruang A203",
+                    Location = "Gedung D4 Lantai 2"
+                },
+                new Room
+                {
+                    id = 2,
+                    Name = "Ruang HH-104",
+                    Location = "Gedung D3 Lantai 1"
+                },
+                new Room
+                {
+                    id = 3,
+                    Name = "Lab Jaringan",
+                    Location = "Gedung D4 Lantai 3"
+                }
+            );
         }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
---
+++ Migrations/20260210075924_SeedInitialData.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _2026_ManajemenRuangan_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomName",
+                table: "RoomBookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "RoomBookings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "Rooms",
+                columns: new[] { "id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Gedung D4 Lantai 2", "Ruang A203" },
+                    { 2, "Gedung D3 Lantai 1", "Ruang HH-104" },
+                    { 3, "Gedung D4 Lantai 3", "Lab Jaringan" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomBookings_RoomId",
+                table: "RoomBookings",
+                column: "RoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoomBookings_Rooms_RoomId",
+                table: "RoomBookings",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoomBookings_Rooms_RoomId",
+                table: "RoomBookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RoomBookings_RoomId",
+                table: "RoomBookings");
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Rooms",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "RoomBookings");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Rooms",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomName",
+                table: "RoomBookings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
