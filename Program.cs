1be7e7ea02316dac67ee4e150fb3d3f78ec7db87
calvinkurniawann <calvinnkurniawan@gmail.com>
2026-02-12 23:26:45 +0700

    refactor(cors): add CORS configuration for frontend integration #14

--- Program.cs
+++ Program.cs
@@ -11,7 +11,17 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=manajemenruangan.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 if (app.Environment.IsDevelopment())
 {
